1cd92e94987ac64560390c1af73a4f286095c0fe
Parin <acharyaparin05@gmail.com>
2026-05-19 01:50:17 +0300

    scraping project stars and adding to dataframe

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach",True)
 
 project_url="https://github.com/collections/machine-learning"
+
 #Created web driver.
 def create_driver():
     driver = webdriver.Chrome(options=chrome_options)
     return driver
+
 #Created a webdriver instance.
 driver1 = create_driver()
 driver1.get(project_url)
@@ -21,25 +23,21 @@ driver1.get(project_url)
 articles = driver1.find_elements(By.TAG_NAME, "article")
 article_names = []
 article_links = []
+article_stars = []
 
 #This for loop section iterates through each article element found on the page, 
 # extracting the link and name of each article and appending them to the respective lists.
 for article in articles: 
     article_links.append(article.find_element(By.CSS_SELECTOR, "h1 > a").get_attribute("href"))
     article_names.append(article.find_element(By.CSS_SELECTOR, "h1 > a").text)
+    article_stars.append(article.find_element(By.CSS_SELECTOR, "a[href*='/stargazers']").text.strip())
 
 df = pd.DataFrame({
     "Name": article_names,
-    "Link": article_links
+    "Link": article_links,
+    "Stars": article_stars
 })
 article_csv = df.to_csv(index=False)
 
-with open(".\projects.csv", mode="a") as file:
+with open("./projects.csv", mode="a") as file:
     file.write(article_csv)
-
-# try: 
-#     stars = project.find_element(By.CSS_SELECTOR, "a[href*='/stargazers']").strip()
-#     print(f"Stars: {stars}") 
-#     project_df["Stars"] = stars # store in the Pandas dataframe
-# except (TimeoutException, StaleElementReferenceException):
-#     print("Error occurred while fetching stars.")   
