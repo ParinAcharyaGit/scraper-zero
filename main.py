3da495f1639b65e0e489d0f1689ced9f243524d6
Parin <acharyaparin05@gmail.com>
2026-05-19 01:09:51 +0300

    logic for scraping project star count

--- main.py
+++ main.py
@@ -13,3 +13,10 @@ Project_Url="https://github.com/collections/machine-learning"
 def create_driver(url):
     driver = webdriver.Chrome(options=chrome_options)
     return driver
+
+# try: 
+#     stars = project.find_element(By.CSS_SELECTOR, "a[href*='/stargazers']").strip()
+#     print(f"Stars: {stars}") 
+#     project_df["Stars"] = stars # store in the Pandas dataframe
+# except (TimeoutException, StaleElementReferenceException):
+#     print("Error occurred while fetching stars.")   
